a35c4d31d42ab97f4cd87f681393e3906962d185
Anita SV <anitasvasu@gmail.com>
2018-02-01 00:58:06 +0530

    Update concealer.js

--- concealer.js
+++ concealer.js
@@ -5,47 +5,76 @@ function concealer(cons) {
         return Array.prototype.slice.call(args)
     }
 
-    function handleFunction(closure, proto) {
-        return function() {
-            return proto.apply(closure, argsArray(arguments))
+    function isPrivate(prop) {
+        return typeof(prop) == 'string' && prop.startsWith('_')
+    }
+
+    function trapPrivate(prop, closure, action, ret) {
+        var proto = closure[prop]
+        var protoType = typeof(proto)
+        if (isPrivate(prop)) {
+            if (protoType == 'function') {
+                console.error('Tried to get private method', prop)
+            } else {
+                console.error('Tried to get private field', prop)
+            }
+            return ret
+        } else {
+            return action(proto, protoType)
         }
     }
 
-    var newCons = function() {
-        var closure = {}
-        cons.apply(closure, argsArray(arguments));
+    function shouldForward(prop) {
+        if (prop == 'hasOwnProperty') {
+            return false
+        } else {
+            return true
+        }
+    } 
+
+    const newCons = function() {
+        const initial = {}
+        const closure = cons.apply(initial, argsArray(arguments)) || initial
         Object.setPrototypeOf(closure, cons.prototype)
 
-        var p = new Proxy(closure, {
-            get: function(target, prop) {
-                if (prop.startsWith('_')) {
-                    if (typeof(closure[prop]) == 'function') {
-                        console.error('Tried to get private method', prop)
-                    } else {
-                        console.error('Tried to get private field', prop)
-                    }
-                    return
+        const p = new Proxy(closure, {
+            get(target, prop) {
+                if (shouldForward(prop)) {
+                    return trapPrivate(prop, closure, function(proto, protoType) {
+                        if (protoType == 'function') {
+                            return proto.bind(closure)
+                        } else {
+                            return proto
+                        }
+                    })
                 } else {
-                    var proto = closure[prop]
-                    if (typeof(proto) == 'function') {
-                        return handleFunction(closure, proto)
-                    } else {
-                        return proto
-                    }
+                    return Reflect.get(target, prop)
                 }
             },
-            set: function(target, prop, value) {
-                if (prop.startsWith('_')) {
-                    if (typeof(closure[prop]) == 'function') {
-                        console.error('Tried to override private method', prop)
-                    } else {
-                        console.error('Tried to set private field', prop)
-                    }                    
-                    return
+            set(target, prop, value) {
+                if (shouldForward(prop)) {
+                    return trapPrivate(prop, closure,  function(proto, protoType) {
+                        closure[prop] = value
+                    })
                 } else {
-                    closure[prop] = value
+                    return Reflect.set(target, prop)
                 }
             },
+            has(target, prop) {
+                if (!isPrivate()) {
+                    return Reflect.has(closure, prop)
+                } else {
+                    return false
+                }
+            },
+            ownKeys(target) {
+                return Object.keys(closure).filter((x) => !isPrivate(x))
+            },
+            getOwnPropertyDescriptor(target, prop) {
+                if (!isPrivate(prop)) {
+                  return Reflect.getOwnPropertyDescriptor(closure, prop);
+                }
+            }
         });
 
         return p;
